9537513d41d94c034bb77f18936eafcd9c404eab
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-09-03 14:52:18 -0500

    action creators for dispatching add/delete/toggle task

---
+++ src/action-creators/index.js
@@ -0,0 +1,9 @@
+import addTask from './add-task';
+import deleteTask from './delete-task';
+import toggleDone from './toggle-done';
+
+export default {
+    addTask,
+    deleteTask,
+    toggleDone
+};
